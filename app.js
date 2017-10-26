49fa756dfa715d3d0644760287b9a63100236a04
Natalie Ventura Villasana <nat@storj.io>
2017-10-26 14:15:46 -0400

    adds bucketList var

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var bucketList = require('./routes/bucketList');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/bucketList', bucketList);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
