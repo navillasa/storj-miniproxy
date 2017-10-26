3a1b44f7ddcccf01c3f4c9a1cdb22e92aa44f58c
Natalie Ventura Villasana <nat@storj.io>
2017-10-26 16:15:00 -0400

    adds libstorj dependency, adds getInfo and getBuckets methods to bucketList

--- routes/bucketList.js
+++ routes/bucketList.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+// console logs for info(?) and buckets
+storj.getInfo((err, res) => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log('info:', result);
+
+  storj.getBuckets((err, res) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('buckets:', res);
+    storj.destroy();
+  });
+});
+
 router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
+
+
 
 module.exports = router;
