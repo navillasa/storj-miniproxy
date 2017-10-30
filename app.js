bbf3095edb2fc938866f78d33685445163c3a973
Natalie Ventura Villasana <nat@storj.io>
2017-10-30 10:58:06 -0400

    comments out createBucket and uploadDownload-- gonna try to get one working first

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ require('dotenv').config();
 
 var index = require('./routes/index');
 var bucketList = require('./routes/bucketList');
-var createBucket = require('./routes/createBucket');
-var uploadDownload = require('./routes/uploadDownload');
+// var createBucket = require('./routes/createBucket');
+// var uploadDownload = require('./routes/uploadDownload');
 
 var app = express();
 
@@ -23,9 +23,6 @@ const storj = new Environment({
   encryptionKey: process.env.ENCRYPT_KEY,
   logLevel: 0
 }); 
-
-// bucketList
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -48,8 +45,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/bucketList', bucketList);
-app.use('/createBucket', createBucket);
-app.use('/uploadDownload', uploadDownload);
+// app.use('/createBucket', createBucket);
+// app.use('/uploadDownload', uploadDownload);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
