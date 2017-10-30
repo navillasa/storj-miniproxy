c9b53d178ab962fa06d6800586f8982f6adcd12f
Natalie Ventura Villasana <nat@storj.io>
2017-10-30 11:15:09 -0400

    adds nodemon as dependency and fixes typo bug in bucketList

--- routes/bucketList.js
+++ routes/bucketList.js
@@ -1,28 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-// grabs var storj from app
+// grabs `storj` variable from app
 router.get('/', (req, res, next) => {
   var storj = req.storj;
   console.log(storj);
 
-// console logs for info(?) and buckets
-// hmm for some reason the var storj here is still undefined
-// need to figure out
-storj.getInfo((err, res) => {
-  if (err) {
-    return console.error(err);
-  }
-  console.log('info:', result);
-
-  storj.getBuckets((err, res) => {
+  // console logs for info(?) and buckets
+  storj.getInfo((err, res) => {
     if (err) {
       return console.error(err);
     }
-    console.log('buckets:', res);
-    storj.destroy();
+    console.log('info:', res);
+
+    storj.getBuckets((err, res) => {
+      if (err) {
+        return console.error(err);
+      }
+      console.log('buckets:', res);
+      storj.destroy();
+    });
   });
-});
 
 });
+
 module.exports = router;
