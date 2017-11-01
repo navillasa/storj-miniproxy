a20620fb889f8144422e4ce075f82c61c2eefbe0
Natalie Ventura Villasana <nat@storj.io>
2017-11-01 14:42:41 -0400

    establishes socket connection

--- public/javascripts/clientSocket.js
+++ public/javascripts/clientSocket.js
@@ -1,1 +1,10 @@
-console.log('meep beep');
+let ws = new WebSocket('ws://localhost:9000');
+
+ws.onopen = () => {
+  console.log('open')
+}
+
+ws.onmessage = (ev) => {
+  let _data = JSON.parse(ev.data);
+  console.log(_data);
+}
--- serverSocket.js
+++ serverSocket.js
@@ -5,12 +5,23 @@ class ServerSocket {
   constructor(wss, ws) {
     console.log('ws init ran');
 
-    wss.on('connection', (socket) => {
-      ws.on('message', (msg) => {
-        let parsedData = JSON.parse(data);
-        console.log(parsedData);
-      });
-    });
+    // wss.on('connection', (socket) => {
+    //  ws.on('message', (msg) => {
+    //    let parsedData = JSON.parse(data);
+    //    console.log(parsedData);
+    //  });
+    // });
+
+    wss.on('connection', (ws) => {
+      console.log('websocket connection open');
+
+      if (ws.readyState === ws.OPEN) {
+        ws.send(JSON.stringify({
+          msg1: 'hello i\'m message 1'
+        }))
+      }
+   });
+        
 
   }
 }
