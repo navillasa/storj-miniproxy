cd7475bbf7edf714bc2637deecf33c8aa431ff64
Natalie Ventura Villasana <nat@storj.io>
2017-11-06 18:37:03 -0500

    adds storj method storeFile, now getting Error: File read error at Error (native); the POST of the target upload is working though

--- routes/bucketList.js
+++ routes/bucketList.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { Environment } = require('storj');
+const storj = new Environment({
+  bridgeUrl: process.env.BRIDGE_URL,
+  bridgeUser: process.env.BRIDGE_EMAIL,
+  bridgePass: process.env.BRIDGE_PASS,
+  encryptionKey: process.env.ENCRYPT_KEY,
+  logLevel: 4
+});
 
 // grabs `storj` variable from app
 router.get('/', (req, res, next) => {
@@ -27,10 +35,31 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/:bucketId', (req, res, next) => {
-  var bucketId = req.params.bucketId;
+  let bucketId = req.params.bucketId;
   res.render('bucket', {
     layout: 'layout',
     id: bucketId
+  });
+})
+
+router.post('/:bucketId/upload/', (req, res) => {
+  let bucketId = req.params.bucketId;
+  storj.storeFile(bucketId, 'uploads/', {
+    filename: 'test.jpg',
+    progressCallback: (progress, uploadedBytes, totalBytes) => {
+      console.log('Progress: %d, UploadedBytes: %d, TotalBytes: %d',
+          progress, uploadedBytes, totalBytes);
+    },
+    finishedCallback: (err, fileId) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log('File upload complete: ', fileId);
+      res.json({
+        success: true,
+        message: 'image uploaded :)'
+      });
+    }
   });
 });
 
