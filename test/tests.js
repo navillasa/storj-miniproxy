117be3afd46cb517af4a6586844535b417baf2ed
Natalie Ventura Villasana <nat@storj.io>
2017-11-13 18:12:58 -0500

    adds a few endpoint harnesses to tests

--- test/tests.js
+++ test/tests.js
@@ -6,18 +6,18 @@ const chai = require('chai');
 const expect = require('chai').expect;
 const app = require('../app');
 
-chai.use(require('chai-http'));
-
 describe('loading express', function () {
   let server;
   beforeEach(function () {
     server = require('../app');
   });
+
   it('responds to /', function testSlash(done) {
     request(server)
       .get('/')
       .expect(200, done);
   });
+
   it('should 404 everything else', function testPath(done) {
     request(server)
       .get('/sample/example')
@@ -25,15 +25,13 @@ describe('loading express', function () {
   });
 });
 
-describe('GET /buckets', function () {
+describe('GET /bucketList', function () {
   it('should call getBuckets', function () {
     request(app)
       .get('/bucketList')
-      .expect('Content-Length', '4')
-      .expect('Content-Type', 'json')
       .end((err, res) => {
         if (err) throw err; 
-        console.log('res: ', res);
+        console.log('****res for bucketList is:', res);
       });
   });
 });
@@ -44,9 +42,20 @@ describe('GET /bucketList/:id', function () {
       .get('/bucketList/e9980f248d1f5b62802e310a')
       .end((err, res) => {
         if (err) throw err;
-        console.log('res.body', res.body);
+        console.log('****res.body for GET /bucketList/:id is', res.body);
         done();
       }); 
   });
 });
 
+describe('GET /bucketList/createBucket', function () {
+  it('should create a bucket', (done) => {
+    let bucketId = 'e9980f248d1f5b62802e310a';
+    request(app)
+      .get(`/bucketList/${bucketId}`)
+      .end((err, res) => {
+        if (err) throw err;
+        done();
+      });
+  });
+});
