0d4c4c668dfb16a6daf2441f654af480313a421a
Natalie Ventura Villasana <nat@storj.io>
2017-11-16 18:13:47 -0500

    fixes deleteFile test to use different bucketId than uploadFile test-- all tests are now passing\!\!\!\!\! 😭

--- test/tests.js
+++ test/tests.js
@@ -74,6 +74,7 @@ describe('Tests for GET /createBucket', function () {
 });
 
 describe('Tests for POST /bucketList/:bucketId', function () {
+  this.timeout(7000);
   it('should send chosen file to local server', (done) => {
     let bucketId = 'e9980f248d1f5b62802e310a';
     request(app)
@@ -85,37 +86,47 @@ describe('Tests for POST /bucketList/:bucketId', function () {
         // console.log('posting test.jpg res:::::::', res);
         let queryStr = res.header.location;
         let bucketId = queryStr.substring(queryStr.indexOf('?') + 9, queryStr.indexOf('?') + 33);
-        console.log('HELLO IM THE BUCKETID', bucketId);
         done();
       });
   });
 });
 
 describe('Tests for GET /bucketList/:bucketId/:fileId/deleteFile', function () {
+  this.timeout(7000);
   it('should delete file within bucket', (done) => {
-    let bucketId = 'e9980f248d1f5b62802e310a';
+    let bucketId = 'e995ee7cd50855b04ba2528a';
     request(app)
-      .get(`/bucketList/${bucketId}/fakeFile`)
+      .post(`/bucketList/${bucketId}`)
+      .attach('file', 'test/test2.jpg')
+      .expect(302)
       .end((err, res) => {
         if (err) throw err;
-        done();
-      })
+        // console.log('posting test.jpg res:::::::', res);
+        let queryStr = res.header.location;
+        let fileId = queryStr.substring(queryStr.indexOf('?') + 9, queryStr.indexOf('?') + 33);
+        request(app)
+          .get(`/bucketList/${bucketId}/${fileId}/deleteFile`)
+          .expect(302)
+          .end((err, res) => {
+            done();
+          });
+      });
   });
 });
 
-// describe('Tests for GET /bucketList/:bucketId/:fileId/download', function () {
-//   it('should download file within bucket', (done) => {
-//     let bucketId = 'e9980f248d1f5b62802e310a';
-//     let fileId = '1aab03a27cf38e34d9cbb703';
-//     request(app)
-//       .get(`/bucketList/${bucketId}/${fileId}/download`)
-//       .end((err, res) => {
-//         if (err) throw err;
-//         // console.log('****res.body for file download', res.body);
-//         done();
-//       });
-//   });
-// });
+describe('Tests for GET /bucketList/:bucketId/:fileId/download', function () {
+  it('should download file within bucket', (done) => {
+    let bucketId = 'e9980f248d1f5b62802e310a';
+    let fileId = '3da48174708033faf9985584';
+    request(app)
+      .get(`/bucketList/${bucketId}/${fileId}/download`)
+      .end((err, res) => {
+        if (err) throw err;
+        // console.log('****res.body for file download', res.body);
+        done();
+      });
+  });
+});
 
 describe('Tests for GET /bucketList/:bucketId/deleteBucket', function () {
   it('should delete specified bucket', (done) => {
